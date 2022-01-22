9e4692bde0975f2f63ca3b46ddec4dcd5f1d7402
Caleb Dauphin <calebdauphindev@gmail.com>
2022-01-22 08:00:24 -0500

    Add dynamic weather icon and alt text using api

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function dateAndTime() {
-  let li = document.getElementById('date');
   let date = new Date();
   let hour = date.getHours();
   let minutes = date.getMinutes();
@@ -64,9 +63,13 @@ function displayTemperature(response) {
   windEl.innerHTML = Math.round(response.data.wind.speed);
   let dateEl = document.getElementById("date");
   dateEl.innerHTML = dateAndTime(response.data.dt * 1000);
+  let iconEl = document.getElementById("icon");
+  iconEl.setAttribute("src", ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconEl.setAttribute("alt", response.data.weather[0].description);
 }
-
+let city = "New York";
 let apiKey = "f4d989e0a37469e143375a913c800d40";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 
 axios.get(apiUrl).then(displayTemperature);
